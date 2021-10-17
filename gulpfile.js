44d16031530e96c15c439b0b323dfcb2135c4fcf
iamirinad <irka_dimcheva@abv.bg>
2021-10-18 02:03:39 +0300

    Gulpfile deploy task

--- gulpfile.js
+++ gulpfile.js
@@ -40,4 +40,5 @@ gulp.task('watch', function () {
   gulp.watch('docs/**/*.js').on('change', browserSync.reload);
 });
 
+gulp.task('deploy', gulp.series('jekyll', 'css'));
 gulp.task('default', gulp.series('jekyll', 'css', 'watch'));
